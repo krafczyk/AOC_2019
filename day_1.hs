5676572ca7a2d1762bd3af4bfa8eebb31ffac840
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-11-30 23:23:05 -0600

    Complete task 2 day 1!

--- day_1.hs
+++ day_1.hs
@@ -8,12 +8,20 @@ argDefinitions = [ ("input_filepath", ["-i", "--input-file"], "Filepath to use f
 fuelConvert :: Int -> Int
 fuelConvert mass = (mass `div` 3) - 2
 
+fuelSeries :: Int -> [Int] -> [Int]
+fuelSeries mass xs
+    | mass <= 6 = xs -- All fuel less than or equal to 6 requires no more fuel.
+    | otherwise = let next_fuel = fuelConvert mass in
+                  fuelSeries next_fuel (next_fuel:xs)
+
 handleFile :: String -> IO ()
 handleFile input_filepath = withFile input_filepath ReadMode (\handle -> do
                                                                          file_data <- hGetContents handle
                                                                          let mass_data = map (\x -> read x :: Int) $ lines file_data
                                                                              fuel_data = map fuelConvert mass_data
-                                                                         putStrLn $ "Task 1: " ++ (show $ sum fuel_data))
+                                                                         putStrLn $ "Task 1: " ++ (show $ sum fuel_data)
+                                                                         let fuel_data_2 = map (\m -> sum $ fuelSeries m []) mass_data
+                                                                         putStrLn $ "Task 2: " ++ (show $ sum fuel_data_2))
 
 argHelpHandler progArgs args = if AP.helpPresent progArgs args
                                    then AP.writeHelpText progArgs
