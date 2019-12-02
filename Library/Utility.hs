9b359e2629b536118d5a29f1cd2011850a785bc8
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-12-02 10:56:52 -0600

    Move some utility functions to the utility library.

---
+++ Library/Utility.hs
@@ -0,0 +1,11 @@
+module Library.Utility where
+
+replaceNth :: Int -> a -> [a] -> [a]
+replaceNth _ _ [] = []
+replaceNth n newVal (x:xs)
+    | n == 0 = newVal:xs
+    | otherwise = x:replaceNth (n-1) newVal xs
+
+takeWhileInclusive :: (a -> Bool) -> [a] -> [a]
+takeWhileInclusive _ [] = []
+takeWhileInclusive p (x:xs) = x : if p x then takeWhileInclusive p xs else []
--- day_2.hs
+++ day_2.hs
@@ -2,15 +2,10 @@ import System.Environment
 import System.IO
 import qualified Data.Map as Map
 import qualified Library.ArgParser as AP
+import qualified Library.Utility as U
 import Data.List.Split
 
 argDefinitions = [ ("input_filepath", ["-i", "--input-file"], "Filepath to use for input", 1) ]
-
-replaceNth :: Int -> a -> [a] -> [a]
-replaceNth _ _ [] = []
-replaceNth n newVal (x:xs)
-    | n == 0 = newVal:xs
-    | otherwise = x:replaceNth (n-1) newVal xs
 
 type Inst3 = Int -> Int -> Int -> [Int] -> [Int]
 type IntcodeState = (Int, [Int])
@@ -23,12 +18,11 @@ applyInst3 func a b c state
     | otherwise = Just (func a b c state)
     where state_length = length state
 
-
 addInst :: Inst3
-addInst a b c state = replaceNth c ((state !! a)+(state !! b)) state
+addInst a b c state = U.replaceNth c ((state !! a)+(state !! b)) state
 
 multInst :: Inst3
-multInst a b c state = replaceNth c ((state !! a)*(state !! b)) state
+multInst a b c state = U.replaceNth c ((state !! a)*(state !! b)) state
 
 advanceStateFunc3 :: Inst3 -> IntcodeState -> Maybe [Int]
 advanceStateFunc3 func (idx, state) = applyInst3 func (state !! (idx+1)) (state !! (idx+2)) (state !! (idx+3)) state
@@ -50,17 +44,12 @@ advanceCondition :: Maybe IntcodeState -> Bool
 advanceCondition Nothing = False
 advanceCondition (Just (idx, state)) = if (state !! idx) == 99 then False else True
 
-takeWhileInclusive :: (a -> Bool) -> [a] -> [a]
-takeWhileInclusive _ [] = []
-takeWhileInclusive p (x:xs) = x : if p x then takeWhileInclusive p xs else []
 
 fileHandler handle = do
                      file_data <- hGetContents handle
                      let intcode_prog = map (\x -> read x :: Int) $ splitOn "," (lines file_data !! 0)
-                     --putStrLn $ show $ takeWhileInclusive advanceCondition $ iterate (>>=advanceState) (Just (0, intcode_prog)) --  This debug section is great for testing the program engine works properly.
-                     -- We modify the initial program.
-                     let task_1_prog = replaceNth 1 12 $ replaceNth 2 2 intcode_prog
-                         final_state = (take 1 $ reverse $ takeWhileInclusive advanceCondition $ iterate (>>=advanceState) (Just (0, task_1_prog))) !! 0
+                     let task_1_prog = U.replaceNth 1 12 $ U.replaceNth 2 2 intcode_prog
+                         final_state = (take 1 $ reverse $ U.takeWhileInclusive advanceCondition $ iterate (>>=advanceState) (Just (0, task_1_prog))) !! 0
                      case final_state of
                          Nothing -> putStrLn "Task 1 failed!"
                          Just (idx, state) -> putStrLn $ "Task 1: " ++ show (state !! 0)
