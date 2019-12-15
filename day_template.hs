ec56b9bee6f5578934eaf924d9d0a4f703b5f35d
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-12-15 08:27:01 -0600

    Change template formatting a bit.

--- day_template.hs
+++ day_template.hs
@@ -6,29 +6,33 @@ import qualified Library.ArgParser as AP
 argDefinitions = [ ("input_filepath", ["-i", "--input-file"], "Filepath to use for input", 1) ]
 
 fileHandler :: AP.ArgMap -> System.IO.Handle -> IO ()
-fileHandler argMap handle = do
-                            file_data <- hGetContents handle
-                            putStrLn file_data -- Read problem data here
+fileHandler argMap handle =
+    do
+    file_data <- hGetContents handle
+    putStrLn file_data -- Read problem data here
 
 handleFile :: String -> AP.ArgMap -> IO ()
 handleFile input_filepath argMap = withFile input_filepath ReadMode (fileHandler argMap)
 
 handleArguments :: AP.ArgMap -> IO ()
-handleArguments argMap = case Map.lookup "input_filepath" argMap of
-                             Just x -> let input_filepath = (reverse x !! 0) !! 0 in
-                                       handleFile input_filepath argMap
-                             Nothing -> putStrLn "Couldn't get input_filepath"
+handleArguments argMap =
+    case Map.lookup "input_filepath" argMap of
+        Just x -> let input_filepath = (reverse x !! 0) !! 0 in
+                  handleFile input_filepath argMap
+        Nothing -> putStrLn "Couldn't get input_filepath"
 
-argHelpHandler progArgs args = if AP.helpPresent progArgs args
-                                   then AP.writeHelpText progArgs
-                                   else let parse_result = AP.parseArguments progArgs args in
-                                       case parse_result of
-                                            Left msg -> putStrLn msg
-                                            Right argMap -> handleArguments argMap
+argHelpHandler progArgs args =
+    if AP.helpPresent progArgs args
+        then AP.writeHelpText progArgs
+        else let parse_result = AP.parseArguments progArgs args in
+             case parse_result of
+                 Left msg -> putStrLn msg
+                 Right argMap -> handleArguments argMap
 
-main = do
-       args <- getArgs
-       let progArgCont = AP.buildProgramArguments "Day template" argDefinitions
-       case progArgCont of
-           Left msg -> putStrLn msg
-           Right progArgs -> argHelpHandler progArgs args
+main =
+    do
+    args <- getArgs
+    let progArgCont = AP.buildProgramArguments "Day template" argDefinitions
+    case progArgCont of
+        Left msg -> putStrLn msg
+        Right progArgs -> argHelpHandler progArgs args
