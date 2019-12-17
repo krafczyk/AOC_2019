3d370ef8cd0ab126c3bf72e31a775170a76e841d
Matthew Krafczyk <krafczyk.matthew@gmail.com>
2019-12-17 05:01:42 -0600

    Solve day 16 task 2.

--- day_16.hs
+++ day_16.hs
@@ -24,9 +24,27 @@ task1 :: [Int] -> Int -> String
 task1 message_data num =
     get8Digits 0 $ run_patterns message_data num
 
+iteration :: Int -> [Int] -> [Int]
+iteration len input =
+    foldr (\i acc-> ((sum $ take i input) `mod` 10):acc) [] [1..len]
+
+iteration2 :: Int -> [Int] -> [Int]
+iteration2 len input =
+    --foldr (\v acc-> (((head acc)+v) `mod` 10):acc) [last input] (take ((length input)-1) input)
+    reverse $ foldl (\(l:acc) v -> ((l+v) `mod` 10):l:acc) [head input] (drop 1 input)
+
 task2 :: [Int] -> Int -> String
-task2 message_data num = get8Digits message_offset $ run_patterns (take (10000*(length message_data)) $ cycle message_data) num
+task2 message_data num
+    | message_offset < (total_len `div` 2) = "Can't retrieve message in this part of message."
+    | otherwise = (get8Digits 0 $ reverse final_it2)
+    -- Extract message offset from the start of the message.
     where message_offset = sum $ map (\(v,e) -> v*(10^e)) $ zip (reverse $ take 7 message_data) [0..]
+          total_len = 10000*(length message_data)
+          count_back = total_len-message_offset
+          rev_msg = take count_back $ cycle $ reverse message_data
+          msg = reverse rev_msg
+          final_it = head $ drop num $ iterate (iteration count_back) rev_msg
+          final_it2 = head $ drop num $ iterate (iteration2 count_back) rev_msg
 
 fileHandler :: AP.ArgMap -> System.IO.Handle -> IO ()
 fileHandler argMap handle =
@@ -34,7 +52,7 @@ fileHandler argMap handle =
     file_data <- hGetContents handle
     let message_data = map (\x -> (ord x)-(ord '0')) $ head $ lines file_data
     putStrLn $ "Task 1: " ++ (task1 message_data 100)
-    --putStrLn $ "Task 2: " ++ (task2 message_data 2)
+    putStrLn $ "Task 2: " ++ (task2 message_data 100)
 
 handleFile :: String -> AP.ArgMap -> IO ()
 handleFile input_filepath argMap = withFile input_filepath ReadMode (fileHandler argMap)
